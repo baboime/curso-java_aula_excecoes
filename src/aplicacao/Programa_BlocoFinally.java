441e86ca5dcbc5123c87e1678a2cc6566f1a362d
Edney Baboim <ednney@gmail.com>
2020-04-05 13:48:22 -0300

    Bloco Finally

---
+++ src/aplicacao/Programa_BlocoFinally.java
@@ -0,0 +1,29 @@
+package aplicacao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Programa_BlocoFinally {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
+		} 
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Bloco Finally Executado");
+		}
+	}
+}
